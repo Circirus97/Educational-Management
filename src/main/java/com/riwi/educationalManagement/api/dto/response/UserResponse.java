72589a9ba50a564f34ed01fbb51ca90efb935ba5
Circirus97 <msuarezalejandra@gmail.com>
2024-05-31 09:24:39 -0500

    actualizacion

--- src/main/java/com/riwi/educationalManagement/api/dto/response/UserResponse.java
+++ src/main/java/com/riwi/educationalManagement/api/dto/response/UserResponse.java
@@ -11,10 +11,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserResponse {
+
     private Long id;
+
     private String username;
+
     private String password;
+
     private String email;
+
     private String fullName;
+
     private Role role;
+
 }
